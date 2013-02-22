openharmony/third_party_curl

commit 411e809fcbc04409c12f0017353ccb7327e1039a

imap: Added support for the STARTTLS capability (Part One)
Introduced detection of the STARTTLS capability, in order to add support
for TLS upgrades without unconditionally sending the STARTTLS command.

lib/imap.c
@@ -371,8 +371,12 @@ static int imap_endofresp(struct pingpong *pp, int *resp)
               line[wordlen] != '\n';)
           wordlen++;
 
+        /* Does the server support the STARTTLS capability? */
+        if(len >= 8 && !memcmp(line, "STARTTLS", 8))
+          imapc->tls_supported = TRUE;
+
         /* Has the server explicitly disabled clear text authentication? */
-        if(wordlen == 13 && !memcmp(line, "LOGINDISABLED", 13))
+        else if(wordlen == 13 && !memcmp(line, "LOGINDISABLED", 13))
           imapc->login_disabled = TRUE;
 
         /* Does the server support the SASL-IR capability? */

lib/imap.h
@@ -62,6 +62,7 @@ struct imap_conn {
   int cmdid;              /* Last used command ID */
   char resptag[5];        /* Response tag to wait for */
   bool ssldone;           /* Is connect() over SSL done? */
+  bool tls_supported;     /* StartTLS capability supported by server */
   bool login_disabled;    /* LOGIN command explicitly disabled by server */
   bool ir_supported;      /* Initial response supported by server */
 };

commit 714c7701f86683bb8258138a82256bb522a6a056

imap.c: Fixed an incorrect variable reference
Fixed an incorrect variable reference which was introduced in commit
411e809fcbc0 as a result of a copy and paste from SMTP/POP3.

lib/imap.c
@@ -378,7 +378,7 @@ static bool imap_endofresp(struct connectdata *conn, char *line, size_t len,
           wordlen++;
 
         /* Does the server support the STARTTLS capability? */
-        if(len >= 8 && !memcmp(line, "STARTTLS", 8))
+        if(wordlen >= 8 && !memcmp(line, "STARTTLS", 8))
           imapc->tls_supported = TRUE;
 
         /* Has the server explicitly disabled clear text authentication? */