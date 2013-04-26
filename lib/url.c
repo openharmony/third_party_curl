openharmony/third_party_curl

commit 2bcf399bfc4607b0def96bcd77e3144be0031a77

multi: progress function abort must close connection
When the progress function returns to cancel the request, we must mark
the connection to get closed and it must do to the DONE state.

do_init() must be called as early as possible so that state variables
for new connections are reset early. We could otherwise see that the old
values were still there when a connection was to be disconnected very
early and it would make it behave wrongly.

Bug: http://curl.haxx.se/mail/lib-2011-10/0006.html
Reported by: Vladimir Grishchenko

lib/url.c
@@ -136,6 +136,7 @@ int curl_win32_idn_to_ascii(const char *in, char **out);
 static long ConnectionKillOne(struct SessionHandle *data);
 static void conn_free(struct connectdata *conn);
 static void signalPipeClose(struct curl_llist *pipeline, bool pipe_broke);
+static CURLcode do_init(struct connectdata *conn);
 
 /*
  * Protocol table.
@@ -4988,6 +4989,9 @@ static CURLcode create_conn(struct SessionHandle *data,
     ConnectionStore(data, conn);
   }
 
+  /* Setup and init stuff before DO starts, in preparing for the transfer. */
+  do_init(conn);
+
   /*
    * Setup whatever necessary for a resumed transfer
    */
@@ -5333,9 +5337,6 @@ CURLcode Curl_do(struct connectdata **connp, bool *done)
   CURLcode result=CURLE_OK;
   struct connectdata *conn = *connp;
   struct SessionHandle *data = conn->data;
-
-  /* setup and init stuff before DO starts, in preparing for the transfer */
-  do_init(conn);
 
   if(conn->handler->do_it) {
     /* generic protocol-specific function pointer set in curl_connect() */

lib/multi.c
@@ -1657,8 +1657,14 @@ static CURLMcode multi_runsingle(struct Curl_multi *multi,
       }
       /* if there's still a connection to use, call the progress function */
       else if(easy->easy_conn && Curl_pgrsUpdate(easy->easy_conn)) {
-        easy->result = CURLE_ABORTED_BY_CALLBACK;
-        multistate(easy, CURLM_STATE_COMPLETED);
+        /* aborted due to progress callback return code must close the
+           connection */
+        easy->easy_conn->bits.close = TRUE;
+
+        /* if not yet in DONE state, go there, otherwise COMPLETED */
+        multistate(easy, (easy->state < CURLM_STATE_DONE)?
+                   CURLM_STATE_DONE: CURLM_STATE_COMPLETED);
+        result = CURLM_CALL_MULTI_PERFORM;
       }
     }
   } WHILE_FALSE; /* just to break out from! */

commit e4bdd2b4ab6887821b49573bbb4eec532164c6bd

url: initialize speed-check data for file:// protocol
... in order to prevent an artificial timeout event based on stale
speed-check data from a previous network transfer.  This commit fixes
a regression caused by 2bcf399bfc4607b0def96bcd77e3144be0031a77.

Bug: https://bugzilla.redhat.com/906031

RELEASE-NOTES
@@ -30,6 +30,7 @@ This release includes the following bugfixes:
  o CURL_CHECK_CA_BUNDLE: don't check for paths when cross-compiling [4]
  o Various documentation updates
  o getinfo.c: reset timecond when clearing session-info variables [5]
+ o FILE: prevent an artificial timeout event due to stale speed-check data [6]
  o 
 
 This release includes the following known bugs:
@@ -53,4 +54,4 @@ References to bug reports and discussions on issues:
  [3] = http://curl.haxx.se/mail/lib-2013-02/0102.html
  [4] = http://curl.haxx.se/mail/lib-2013-04/0294.html
  [5] = http://bugs.debian.org/cgi-bin/bugreport.cgi?bug=705783
- [6] = 
+ [6] = https://bugzilla.redhat.com/906031

lib/url.c
@@ -5193,6 +5193,9 @@ static CURLcode create_conn(struct SessionHandle *data,
                           -1, NULL); /* no upload */
     }
 
+    /* since we skip do_init() */
+    Curl_speedinit(data);
+
     return result;
   }
 #endif