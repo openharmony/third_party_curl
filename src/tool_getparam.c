openharmony/third_party_curl

commit 01e0a713b1797959fc23743fdd263795bb66d572

curl: add --http1.1 and --http2.0 options

src/tool_getparam.c
@@ -176,7 +176,9 @@ static const struct LongShort aliases[]= {
   {"$J", "metalink",                 FALSE},
   {"$K", "sasl-ir",                  FALSE},
   {"$L", "test-event",               FALSE},
-  {"0",  "http1.0",                  FALSE},
+  {"00",  "http1.0",                 FALSE},
+  {"01",  "http1.1",                 FALSE},
+  {"02",  "http2.0",                 FALSE},
   {"1",  "tlsv1",                    FALSE},
   {"2",  "sslv2",                    FALSE},
   {"3",  "sslv3",                    FALSE},
@@ -985,9 +987,21 @@ ParameterError getparameter(char *flag,    /* f or -long-flag */
     case '~': /* --xattr */
       config->xattr = toggle;
       break;
-    case '0':
-      /* HTTP version 1.0 */
-      config->httpversion = CURL_HTTP_VERSION_1_0;
+    case '0': /* --http* options */
+      switch(subletter) {
+      case '0':
+        /* HTTP version 1.0 */
+        config->httpversion = CURL_HTTP_VERSION_1_0;
+        break;
+      case '1':
+        /* HTTP version 1.1 */
+        config->httpversion = CURL_HTTP_VERSION_1_1;
+        break;
+      case '2':
+        /* HTTP version 2.0 */
+        config->httpversion = CURL_HTTP_VERSION_2_0;
+        break;
+      }
       break;
     case '1':
       /* TLS version 1 */

RELEASE-NOTES
@@ -1,7 +1,7 @@
 Curl and libcurl 7.33.0
 
  Public curl releases:         135
- Command line options:         153
+ Command line options:         155
  curl_easy_setopt() options:   201
  Public functions in libcurl:  58
  Known libcurl bindings:       42

docs/curl.1
@@ -113,8 +113,14 @@ same command line option.)
 Make curl display progress as a simple progress bar instead of the standard,
 more informational, meter.
 .IP "-0, --http1.0"
-(HTTP) Forces curl to issue its requests using HTTP 1.0 instead of using its
-internally preferred: HTTP 1.1.
+(HTTP) Tells curl to use HTTP version 1.0 instead of using its internally
+preferred: HTTP 1.1.
+.IP "--http1.1"
+(HTTP) Tells curl to use HTTP version 1.1. This is the internal default
+version. (Added in 7.33.0)
+.IP "--http2.0"
+(HTTP) Tells curl to issue its requests using HTTP 2.0. This requires that the
+underlying libcurl was built to support it. (Added in 7.33.0)
 .IP "-1, --tlsv1"
 (SSL)
 Forces curl to use TLS version 1 when negotiating with a remote TLS server.

src/tool_help.c
@@ -100,6 +100,8 @@ static const char *const helptext[] = {
   "     --hostpubmd5 MD5  "
   "Hex encoded MD5 string of the host public key. (SSH)",
   " -0, --http1.0       Use HTTP 1.0 (H)",
+  "     --http1.1       Use HTTP 1.1 (H)",
+  "     --http2.0       Use HTTP 2.0 (H)",
   "     --ignore-content-length  Ignore the HTTP Content-Length header",
   " -i, --include       Include protocol headers in the output (H/F)",
   " -k, --insecure      Allow connections to SSL sites without certs (H)",

commit f044bb0d7a7796cde3bc5faf5bc11b9806dd3bc9

curl: unbreak --http1.0 again
I broke it in 01e0a713b1

src/tool_getparam.c
@@ -176,7 +176,7 @@ static const struct LongShort aliases[]= {
   {"$J", "metalink",                 FALSE},
   {"$K", "sasl-ir",                  FALSE},
   {"$L", "test-event",               FALSE},
-  {"00",  "http1.0",                 FALSE},
+  {"0",   "http1.0",                 FALSE},
   {"01",  "http1.1",                 FALSE},
   {"02",  "http2.0",                 FALSE},
   {"1",  "tlsv1",                    FALSE},
@@ -989,7 +989,7 @@ ParameterError getparameter(char *flag,    /* f or -long-flag */
       break;
     case '0': /* --http* options */
       switch(subletter) {
-      case '0':
+      case '\0':
         /* HTTP version 1.0 */
         config->httpversion = CURL_HTTP_VERSION_1_0;
         break;