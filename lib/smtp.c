openharmony/third_party_curl

commit 0fc7bc49304f7939ce19d37aa9535195c1610863

smtp: Added support for the conversion of Unix newlines during mail send
Added support for the automatic conversion of Unix newlines to CRLF
during mail uploads.

Feature: http://curl.haxx.se/bug/view.cgi?id=1456

lib/smtp.c
@@ -2320,13 +2320,17 @@ CURLcode Curl_smtp_escape_eob(struct connectdata *conn, ssize_t nread)
   ssize_t si;
   struct SessionHandle *data = conn->data;
   struct SMTP *smtp = data->req.protop;
+  char *scratch = data->state.scratch;
+  char *oldscratch = NULL;
 
   /* Do we need to allocate the scatch buffer? */
-  if(!data->state.scratch) {
-    data->state.scratch = malloc(2 * BUFSIZE);
+  if(!scratch || data->set.crlf) {
+    oldscratch = scratch;
 
-    if(!data->state.scratch) {
-      failf (data, "Failed to alloc scratch buffer!");
+    scratch = malloc(2 * BUFSIZE);
+    if(!scratch) {
+      failf(data, "Failed to alloc scratch buffer!");
+
       return CURLE_OUT_OF_MEMORY;
     }
   }
@@ -2345,7 +2349,7 @@ CURLcode Curl_smtp_escape_eob(struct connectdata *conn, ssize_t nread)
     }
     else if(smtp->eob) {
       /* A previous substring matched so output that first */
-      memcpy(&data->state.scratch[si], SMTP_EOB, smtp->eob);
+      memcpy(&scratch[si], SMTP_EOB, smtp->eob);
       si += smtp->eob;
 
       /* Then compare the first byte */
@@ -2361,17 +2365,17 @@ CURLcode Curl_smtp_escape_eob(struct connectdata *conn, ssize_t nread)
     /* Do we have a match for CRLF. as per RFC-5321, sect. 4.5.2 */
     if(SMTP_EOB_FIND_LEN == smtp->eob) {
       /* Copy the replacement data to the target buffer */
-      memcpy(&data->state.scratch[si], SMTP_EOB_REPL, SMTP_EOB_REPL_LEN);
+      memcpy(&scratch[si], SMTP_EOB_REPL, SMTP_EOB_REPL_LEN);
       si += SMTP_EOB_REPL_LEN;
       smtp->eob = 0;
     }
     else if(!smtp->eob)
-      data->state.scratch[si++] = data->req.upload_fromhere[i];
+      scratch[si++] = data->req.upload_fromhere[i];
   }
 
   if(smtp->eob) {
     /* A substring matched before processing ended so output that now */
-    memcpy(&data->state.scratch[si], SMTP_EOB, smtp->eob);
+    memcpy(&scratch[si], SMTP_EOB, smtp->eob);
     si += smtp->eob;
     smtp->eob = 0;
   }
@@ -2381,11 +2385,19 @@ CURLcode Curl_smtp_escape_eob(struct connectdata *conn, ssize_t nread)
     nread = si;
 
     /* Upload from the new (replaced) buffer instead */
-    data->req.upload_fromhere = data->state.scratch;
+    data->req.upload_fromhere = scratch;
+
+    /* Save the buffer so it can be freed later */
+    data->state.scratch = scratch;
+
+    /* Free the old scratch buffer */
+    Curl_safefree(oldscratch);
 
     /* Set the new amount too */
     data->req.upload_present = nread;
   }
+  else
+    Curl_safefree(scratch);
 
   return CURLE_OK;
 }

lib/transfer.c
@@ -903,15 +903,6 @@ static CURLcode readwrite_upload(struct SessionHandle *data,
       /* store number of bytes available for upload */
       data->req.upload_present = nread;
 
-#ifndef CURL_DISABLE_SMTP
-      if(conn->handler->protocol & PROTO_FAMILY_SMTP) {
-        result = Curl_smtp_escape_eob(conn, nread);
-        if(result)
-          return result;
-      }
-      else
-#endif /* CURL_DISABLE_SMTP */
-
       /* convert LF to CRLF if so asked */
       if((!sending_http_headers) && (
 #ifdef CURL_DO_LINEEND_CONV
@@ -961,6 +952,14 @@ static CURLcode readwrite_upload(struct SessionHandle *data,
       /* We have a partial buffer left from a previous "round". Use
          that instead of reading more data */
     }
+
+#ifndef CURL_DISABLE_SMTP
+    if(conn->handler->protocol & PROTO_FAMILY_SMTP) {
+      result = Curl_smtp_escape_eob(conn, nread);
+      if(result)
+        return result;
+    }
+#endif /* CURL_DISABLE_SMTP */
 
     /* write to socket (send away data) */
     result = Curl_write(conn,

commit 544b0d4d7edc32cd648b37b7460313048fdc6c2d

smtp: Fixed inappropriate free of the scratch buffer
If the scratch buffer was allocated in a previous call to
Curl_smtp_escape_eob(), a new buffer not allocated in the subsequent
call and no action taken by that call, then an attempt would be made to
try and free the buffer which, by now, would be part of the data->state
structure.

This bug was introduced in commit 0fc7bc4930.

lib/smtp.c
@@ -2321,6 +2321,7 @@ CURLcode Curl_smtp_escape_eob(struct connectdata *conn, const ssize_t nread)
   struct SessionHandle *data = conn->data;
   struct SMTP *smtp = data->req.protop;
   char *scratch = data->state.scratch;
+  char *newscratch = NULL;
   char *oldscratch = NULL;
   size_t eob_sent;
 
@@ -2328,8 +2329,8 @@ CURLcode Curl_smtp_escape_eob(struct connectdata *conn, const ssize_t nread)
   if(!scratch || data->set.crlf) {
     oldscratch = scratch;
 
-    scratch = malloc(2 * BUFSIZE);
-    if(!scratch) {
+    scratch = newscratch = malloc(2 * BUFSIZE);
+    if(!newscratch) {
       failf(data, "Failed to alloc scratch buffer!");
 
       return CURLE_OUT_OF_MEMORY;
@@ -2401,7 +2402,7 @@ CURLcode Curl_smtp_escape_eob(struct connectdata *conn, const ssize_t nread)
     data->req.upload_present = si;
   }
   else
-    Curl_safefree(scratch);
+    Curl_safefree(newscratch);
 
   return CURLE_OK;
 }