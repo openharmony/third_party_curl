openharmony/third_party_curl

commit 78fd7931cc2ddeeb2f9359e16125bee2288d42c7

tool_urlglob.c: reverse two loops
By counting from 0 and up instead of backwards like before, we remove
the need for the "funny" check of the unsigned variable when decreased
passed zero. Easier to read and less risk for compiler warnings.

src/tool_urlglob.c
@@ -465,8 +465,7 @@ void glob_cleanup(URLGlob* glob)
   size_t i;
   int elem;
 
-  /* the < condition is required since i underflows! */
-  for(i = glob->size - 1; (i >= 0) && (i < glob->size); --i) {
+  for(i = 0; i < glob->size; i++) {
     if((glob->pattern[i].type == UPTSet) &&
        (glob->pattern[i].content.Set.elements)) {
       for(elem = glob->pattern[i].content.Set.size - 1;
@@ -485,7 +484,6 @@ int glob_next_url(char **globbed, URLGlob *glob)
 {
   URLPattern *pat;
   size_t i;
-  size_t j;
   size_t len;
   size_t buflen = glob->urllen + 1;
   char *buf = glob->glob_buffer;
@@ -498,9 +496,8 @@ int glob_next_url(char **globbed, URLGlob *glob)
     bool carry = TRUE;
 
     /* implement a counter over the index ranges of all patterns,
-       starting with the rightmost pattern */
-    /* the < condition is required since i underflows! */
-    for(i = glob->size - 1; carry && (i >= 0) && (i < glob->size); --i) {
+       starting with the leftmost pattern */
+    for(i = 0; carry && (i < glob->size); i++) {
       carry = FALSE;
       pat = &glob->pattern[i];
       switch (pat->type) {
@@ -512,8 +509,9 @@ int glob_next_url(char **globbed, URLGlob *glob)
         }
         break;
       case UPTCharRange:
-        pat->content.CharRange.ptr_c = (char)(pat->content.CharRange.step +
-                           (int)((unsigned char)pat->content.CharRange.ptr_c));
+        pat->content.CharRange.ptr_c =
+          (char)(pat->content.CharRange.step +
+                 (int)((unsigned char)pat->content.CharRange.ptr_c));
         if(pat->content.CharRange.ptr_c > pat->content.CharRange.max_c) {
           pat->content.CharRange.ptr_c = pat->content.CharRange.min_c;
           carry = TRUE;
@@ -537,8 +535,8 @@ int glob_next_url(char **globbed, URLGlob *glob)
     }
   }
 
-  for(j = 0; j < glob->size; ++j) {
-    pat = &glob->pattern[j];
+  for(i = 0; i < glob->size; ++i) {
+    pat = &glob->pattern[i];
     switch(pat->type) {
     case UPTSet:
       if(pat->content.Set.elements) {

commit 6bf9ac8f89d2f3c24ed4ac9f20f55010fb229992

tool_urlglob.c: partly reverse 78fd7931cc
The loop in glob_next_url() needs to be done backwards to maintain the
logic. 78fd7931cc caused test 1235 to fail.

src/tool_urlglob.c
@@ -495,9 +495,9 @@ int glob_next_url(char **globbed, URLGlob *glob)
   else {
     bool carry = TRUE;
 
-    /* implement a counter over the index ranges of all patterns,
-       starting with the leftmost pattern */
-    for(i = 0; carry && (i < glob->size); i++) {
+    /* implement a counter over the index ranges of all patterns, starting
+       with the rightmost pattern */
+    for(i = glob->size - 1; carry && (i < glob->size); --i) {
       carry = FALSE;
       pat = &glob->pattern[i];
       switch (pat->type) {