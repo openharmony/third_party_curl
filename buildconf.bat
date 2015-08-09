openharmony/third_party_curl

commit e999f4e12480bc1a9e2773fd9a19d1b6ca118aad

buildconf.bat: Added support for file clean-up via -clean

buildconf.bat
@@ -37,7 +37,33 @@ rem snapshot archives.
 
   if not exist GIT-INFO goto nogitinfo
 
+  rem Set our variables
+  setlocal
+  set MODE=GENERATE
+
+:parseArgs
+  if "%~1" == "" goto start
+
+  if /i "%~1" == "-clean" (
+    set MODE=CLEAN
+  ) else (
+    goto unknown
+  )
+
+  shift & goto parseArgs
+
 :start
+  if "%MODE%" == "GENERATE" (
+    call :generate
+  ) else (
+    call :clean
+  )
+
+  goto success
+
+rem Main generate function.
+rem
+:generate
   echo.
   echo Generating prerequisite files
 
@@ -68,12 +94,42 @@ rem snapshot archives.
     cd ..
   )
 
-  goto success
+  exit /B
+
+rem Main clean function.
+rem
+:clean
+  echo.
+  echo Removing prerequisite files
+
+  echo * %CD%\Makefile
+  if exist Makefile (
+    del Makefile
+  )
+
+  echo * %CD%\src\tool_hugehelp.c
+  if exist src\tool_hugehelp.c (
+    del src\tool_hugehelp.c
+  )
+
+  echo * %CD%\include\curl\curlbuild.h
+  if exist include\curl\curlbuild.h (
+    del include\curl\curlbuild.h
+  )
+
+  exit /B
 
 :syntax
   rem Display the help
   echo.
-  echo Usage: buildconf
+  echo Usage: buildconf [-clean]
+  echo.
+  echo -clean    - Removes the files
+  goto error
+
+:unknown
+  echo.
+  echo Error: Unknown argument '%1'
   goto error
 
 :nogitinfo

commit 5878f94b1810951889edcc5554c1909262fdc775

buildconf.bat: Maintain compatibility with DOS based systems
Commit e999f4e124 broke compatibility with DOS and non Windows NT based
versions of Windows due to the use of the setlocal command.

buildconf.bat
@@ -24,14 +24,14 @@ rem ***************************************************************************
 rem NOTES
 rem
 rem This batch file must be used to set up a git tree to build on systems where
-rem there is no autotools support (i.e. Windows).
+rem there is no autotools support (i.e. DOS and Windows).
 rem
 rem This file is not included or required for curl's release archives or daily 
 rem snapshot archives.
 
 :begin
   rem Set our variables
-  setlocal
+  if "%OS%" == "Windows_NT" setlocal
   set MODE=GENERATE
 
   rem Switch to this batch file's directory
@@ -143,9 +143,17 @@ rem
   goto error
 
 :error
-  endlocal
+  if "%OS%" == "Windows_NT" (
+    endlocal
+  ) else (
+    set MODE=
+  )
   exit /B 1
 
 :success
-  endlocal
+  if "%OS%" == "Windows_NT" (
+    endlocal
+  ) else (
+    set MODE=
+  )
   exit /B 0