dscho/curl

vtls: move md5sum into the Curl_ssl struct
The MD5 summing is also an SSL backend-specific function. So let's
include it, offering the previous fall-back code as a separate function
now: Curl_none_md5sum(). To allow for that, the signature had to be
changed so that an error could be returned from the implementation
(Curl_none_md5sum() can run out of memory).

Signed-off-by: Johannes Schindelin <johannes.schindelin@gmx.de>

lib/vtls/axtls.c
@@ -722,7 +722,8 @@ const struct Curl_ssl Curl_ssl_axtls = {
   Curl_none_set_engine,           /* set_engine */
   Curl_none_set_engine_default,   /* set_engine_default */
   Curl_none_engines_list,         /* engines_list */
-  Curl_ssl_false_start            /* false_start */
+  Curl_ssl_false_start,           /* false_start */
+  Curl_none_md5sum                /* md5sum */
 };
 
 const struct Curl_ssl *Curl_ssl = &Curl_ssl_axtls;

openharmony/third_party_curl

lib/vtls/axtls.c
@@ -721,7 +721,8 @@ const struct Curl_ssl Curl_ssl_axtls = {
   Curl_none_set_engine,           /* set_engine */
   Curl_none_set_engine_default,   /* set_engine_default */
   Curl_none_engines_list,         /* engines_list */
-  Curl_none_false_start           /* false_start */
+  Curl_none_false_start,          /* false_start */
+  Curl_none_md5sum                /* md5sum */
 };
 
 const struct Curl_ssl *Curl_ssl = &Curl_ssl_axtls;