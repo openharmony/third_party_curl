openharmony/third_party_curl

commit 240d27fbb030633a4d82e5a447f6ebabcf2dc865

copyright: fix year ranges
Follow-up from c9982bc1953

docs/examples/smtp-vrfy.c
@@ -1,11 +1,11 @@
-/***************************************************************************
+'/***************************************************************************
  *                                  _   _ ____  _
  *  Project                     ___| | | |  _ \| |
  *                             / __| | | | |_) | |
  *                            | (__| |_| |  _ <| |___
  *                             \___|\___/|_| \_\_____|
  *
- * Copyright (C) 1998 - 2019, Daniel Stenberg, <daniel@haxx.se>, et al.
+ * Copyright (C) 1998 - 2020, Daniel Stenberg, <daniel@haxx.se>, et al.
  *
  * This software is licensed as described in the file COPYING, which
  * you should have received as part of this distribution. The terms

commit 0c747448830c0aff4068268e99945ee9e7c6a842

examples: fix comment syntax
Commit 240d27fbb accidentally added a stray character outside of the
comment which broke compilation. Fix by removing.

Reported-by:  autobuild https://curl.se/dev/log.cgi?id=20201105084306-12742

docs/examples/smtp-vrfy.c
@@ -1,4 +1,4 @@
-'/***************************************************************************
+/***************************************************************************
  *                                  _   _ ____  _
  *  Project                     ___| | | |  _ \| |
  *                             / __| | | | |_) | |