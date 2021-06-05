2631cce0697537ae19a6b78bc60a3d088fa3924b
lnlan <lanleinan@163.com>
2021-06-05 15:30:01 +0800

    feat: curl适配m核 【背景】m核应用对curl/mbedtls有需求。
    【修改方案】
    1. BUILD.gn调整，添加m核适配内容
    2. 添加适配m核配置文件，编译时覆盖默认配置
    3. 修复NULL重定义问题
    4. 内核posix新增stdin.c，修复缺少stdin定义问题
    【影响】
    对现有的产品编译不会有影响
    
    re #I3ULAS
    
    Signed-off-by: lanleinan <lanleinan@163.com>
    Change-Id: I63aaf0e45ea7536e2bd72479340535503bd130bd

--- build.sh
+++ build.sh
@@ -5,4 +5,10 @@ set -e
 CURRENT_DIR=$(dirname "$0")
 
 echo "add curl patch..."
-cp -f $CURRENT_DIR/lib/curl_config_lite.h $CURRENT_DIR/lib/curl_config.h
+
+if [ "$1" = "liteos_m" ]; then
+    cp -f $CURRENT_DIR/lib/curl_config_liteos_m.h $CURRENT_DIR/lib/curl_config.h
+else
+    cp -f $CURRENT_DIR/lib/curl_config_liteos_a.h $CURRENT_DIR/lib/curl_config.h
+fi
+
