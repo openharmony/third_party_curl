openharmony/third_party_curl

commit 30bbcd17d58779b84480817e57ef7f8019571978

fix Cygwin/MSYS compilation
_getpid is Windows API. On Cygwin variants it should remain getpid.

Fixes #8220
Closes #9255

lib/smb.c
@@ -34,7 +34,7 @@
 #include <process.h>
 #ifdef CURL_WINDOWS_APP
 #define getpid GetCurrentProcessId
-#elif !defined(MSDOS)
+#elif defined(CURL_WIN32)
 #define getpid _getpid
 #endif
 #endif

commit ab28ec81fc5fc8c4e995a60ca69e28fa18bcb198

smb: replace CURL_WIN32 with WIN32
PR #9255 aimed to fix a Cygwin/MSYS issue (#8220). It used the
`CURL_WIN32` macro, but that one is not defined here, while compiling
curl itself. This patch changes this to `WIN32`, assuming this was the
original intent.

Regression from 30bbcd17d58779b84480817e57ef7f8019571978

Reviewed-by: Marcel Raad

Closes #9701

lib/smb.c
@@ -34,7 +34,7 @@
 #include <process.h>
 #ifdef CURL_WINDOWS_APP
 #define getpid GetCurrentProcessId
-#elif defined(CURL_WIN32)
+#elif defined(WIN32)
 #define getpid _getpid
 #endif
 #endif